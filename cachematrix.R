671755fd7f7a7845889dcc5ef69eca434379ea65
guicadioli <guilherme_cadioli@hotmail.com>
2014-08-23 00:03:25 -0300

    edi

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL  #m will store our 'inverse' and it's reset to NULL
+  set<-function(y){
+    x<<-y 
+    m<<-NULL
+  }
+  get<-function(){x}
+  setsolve<-function(solve)
+  {m<<-solve} #store the value using superassignment
+  getsolve<-function(){m}
+  list(get=get, setsolve=setsolve,getsolve=getsolve)
 
 }
-
-
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+m<-x$getsolve() # accesses the object 'x' and gets the inverse
+if(!is.null(m)){  # if mean was already cached (not NULL) 
+  message("getting cache data")
+  return(m) #return inverse
 }
+data<-x$get()
+m<-solve(data,...)  # if m was NULL then we have to calculate the inverse
+x$setsolve(m)
+m #return inverse
+}
+
+
